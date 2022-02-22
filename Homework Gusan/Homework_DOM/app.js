2540798e6d7c96e0eabbd804ce57a6f646be0db5
Nitro <veronicagushan@gmail.com>
2022-02-22 15:06:24 +0200

    Task 6 (dom)

--- Homework Gusan/Homework_DOM/app.js
+++ Homework Gusan/Homework_DOM/app.js
@@ -4,29 +4,35 @@
 (задачи 1-4 выполнить в одном документе)
 */
 
+/*
 document.addEventListener("DOMContentLoaded", () => {
     document.body.style.backgroundColor = "red";
 });
+*/
 
 /*
 Задача №2 - событие загрузки документа и обращение к элементам DOM
 Через 3 секунды после загрузки страницы, установить цвет фона в зеленый.
 */
 
+/*
 setTimeout(() => {
     document.body.style.backgroundColor = "green";
 }, 3000)
+*/
 
 /*
 Задача №3 - событие click 
 Добавить кнопку с текстом Orange, при нажатии на которой, цвет страницы становится оранжевым.
 */
 
+/*
 const firstButton = document.getElementById("firstButton");
 
 firstButton.addEventListener("click", () => {
     document.body.style.backgroundColor = "orange";
 });
+*/
 
 /*
 Задача №4 - добавление элементов в DOM
@@ -35,6 +41,7 @@ firstButton.addEventListener("click", () => {
 У четных элементов будет зеленый фон а у нечетных фиолетовый.
 */
 
+/*
 let list = document.createElement('ol');
 let addElement = () => {
     let elementOfList = [];
@@ -59,3 +66,35 @@ let addElement = () => {
 
 document.body.append(list);
 addElement();
+*/
+
+/*
+Задача №6 - добавление элементов в DOM
+Добавить в страницу таблицу 8*8. При помощи JS кода закрасить цвет фона чтобы получилась шахматная доска.
+Использовать querySelector*
+*/
+
+function drawBoard() {
+    let board = document.querySelector(".board");
+    let block;
+    let flag = true; // белая-черная клетка
+  
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (j == 0) flag = !flag;
+  
+        block = document.createElement("div");
+  
+        if (flag) block.className = "block black";
+        else block.className = "block white";
+  
+        //вывелa
+        board.appendChild(block);
+  
+        //поменялa с черного на белое
+        flag = !flag;
+      }
+    }
+  }
+  
+  drawBoard();
